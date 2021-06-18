8158cd6eab3f82d11de3436d918c327ea486b9aa
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-18 12:19:17 -0500

    feat: filtering for dance users

---
+++ src/components/practice/DancerCard.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+export const DancerCard = ({ dancer }) => {
+
+
+    return <>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={dancer?.dance_user.img} />
+            <Card.Body>
+                <Card.Title>
+                    {`${dancer?.dance_user.user && dancer?.dance_user.user.first_name} 
+                    ${dancer?.dance_user.user && dancer?.dance_user.user.last_name}`}
+                    <hr></hr>
+                </Card.Title>
+                <Card.Text>
+                    <p>{dancer?.dance_type.label}</p>
+                    <p>{dancer?.role.label}</p>
+                    <p>{dancer?.skill_level.label}</p>
+                    <hr></hr>
+                </Card.Text>
+                <Button variant="primary" href={`./profile/${dancer?.dance_user.id}`}>View Profile</Button>
+            </Card.Body>
+        </Card>
+    </>
+}
--- src/components/practice/FindAPartner.js
+++ src/components/practice/FindAPartner.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react"
 import { useHistory, useParams } from 'react-router-dom';
 import { DanceContext } from '../dance/DanceProvider'
 import { useForm } from "react-hook-form";
+import { DancerCard } from "./DancerCard"
 // import "./dancestyles.css";
 
 //! THIS WAS COPIED FROM ./DANCEFORM    REPURPOSE FOR SEARCH
@@ -23,15 +24,16 @@ export const FindAPartner = () => {
         console.log("new dancers array", dancers)
 
     }, [dancers])
+    
+    
 
     // Capture dance type id value, send to server to find dancers who do that dance
     const onSubmit = (data) => {
         console.log("data", data)
         
-        data.danceTypeId = parseInt(data.danceTypeId)
+        data = parseInt(data.danceTypeId)
         console.log("data after parse", data)
-
-        getDancers(data)
+        data != 0 ? getDancers(data) : alert("Select A Dance Before Submitting ")
     };
 
     console.log(watch("danceTypeId"))
@@ -40,10 +42,11 @@ export const FindAPartner = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
             
             <fieldset>
-                <label for="danceTypeId">Select a Dance: </label>
+                <label for="danceTypeId">Dance: </label>
                 <select {...register("danceTypeId", { required: true })} name= "danceTypeId" id="danceTypeId">
+                    <option key={0} value={0}>Select A Dance</option>
                     {danceTypes?.map(type => {
-                        return <option value={type.id}>{type.label}</option>
+                        return <option key={type.id} value={type.id}>{type.label}</option>
                     })}
                 </select>
             </fieldset>
@@ -51,5 +54,14 @@ export const FindAPartner = () => {
             
             <input type="submit" />
         </form>
+        <section className="results__list">
+            <h3>Dancers:</h3>
+            {
+            dancers.length ? 
+            dancers.map(dancer => {return <DancerCard key={dancer.id} dancer={dancer}/>}) :
+            <p>No dancers yet. Be a trendsetter!</p>
+            }
+
+        </section>
     </>
 }
