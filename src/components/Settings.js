85f61aa0991f3598fe7f315c67db2d1cf36ccfa2
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-23 13:49:46 -0500

    ermoved trailing slash

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -8,5 +8,5 @@ export const apiHeaders = () => {
 }
 
 export const apiSettings = {
-    baseUrl: "https://oneleftfoot-nss-api.herokuapp.com/"
+    baseUrl: "https://oneleftfoot-nss-api.herokuapp.com"
 }
