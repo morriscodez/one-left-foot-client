5da823d80599032e6f49cf474b3994eb11e2cec1
Dylan Morris <dylanrobertmorris@gmail.com>
2021-06-23 14:27:14 -0500

    fix: removed unused imports

--- src/components/dance/PartnerDanceCard.js
+++ src/components/dance/PartnerDanceCard.js
@@ -1,6 +1,5 @@
-import React, { useContext } from "react"
+import React from "react"
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 
 export const PartnerDanceCard = ({ dance }) => {
 
